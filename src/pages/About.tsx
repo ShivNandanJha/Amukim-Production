0559f796fea0c3318809e1057c58ff0aaf018bbf
Abhi Mondal <85216013+AbhiMondal20@users.noreply.github.com>
2024-10-05 14:20:40 +0530

    Initial commit

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -3,7 +3,6 @@ import About from '../assets/about.jpg'
 const AboutUs = () => {
   return (
     <section className="relative bg-black text-white py-12">
-     <div className="relative h-full w-full bg-black"><div className="absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]"></div><div className="absolute left-0 right-0 top-[-10%] h-[1000px] w-[1000px] rounded-full bg-[radial-gradient(circle_400px_at_50%_300px,#fbfbfb36,#000)]"></div></div>
       <div className="container mx-auto flex flex-col md:flex-row items-center justify-center px-4">
         {/* Left Image */}
         <div className="md:w-1/2 mb-8 md:mb-0">
